0332b39f2f746872f87d0c3d1a9411cce2238209
Thomas Couchoud <thomas.couchoud@gmail.com>
2022-03-06 21:39:18 +0100

    Progress by millis

--- src/main/java/fr/raksrinana/mediaconverter/progress/ProgressBarNotifier.java
+++ src/main/java/fr/raksrinana/mediaconverter/progress/ProgressBarNotifier.java
@@ -23,7 +23,7 @@ public class ProgressBarNotifier implements ProgressListener{
 		
 		progressBar.stepTo(0);
 		progressBar.setExtraMessage(filename);
-		progressBar.maxHint(frameCount);
+		progressBar.maxHint(totalDuration.toMillis());
 	}
 	
 	@NotNull
@@ -33,8 +33,8 @@ public class ProgressBarNotifier implements ProgressListener{
 	
 	@Override
 	public void onProgress(FFmpegProgress progress){
-		if(Objects.nonNull(progress.getFrame())){
-			progressBar.stepTo(progress.getFrame());
+		if(Objects.nonNull(progress.getTimeMillis())){
+			progressBar.stepTo(progress.getTimeMillis());
 		}
 		log.debug("{} - {} / {} frames - {} fps - {} / {}",
 				filename,
